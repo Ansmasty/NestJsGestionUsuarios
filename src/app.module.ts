4c32307b9147d613d8d9885c0e6a02cdaa24dd4c
Jorge Manríquez <jorge.manriquez11@inacapmail.cl>
2024-11-22 17:18:46 -0300

    fix: agregar controlador para la ruta raíz y actualizar configuración de Vercel

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -18,5 +20,7 @@ import { AuthModule } from './auth/auth.module';
     UsersModule,
     AuthModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
