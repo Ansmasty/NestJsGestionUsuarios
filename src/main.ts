6507a96f8dd1ce29fe7f4622f33be0cd7af1b92e
Jorge Manríquez <jorge.manriquez11@inacapmail.cl>
2024-11-22 17:57:24 -0300

    fix: mejorar configuración de Swagger para resolver problema de página en blanco

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,11 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
